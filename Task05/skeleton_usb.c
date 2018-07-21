27ca03844436f49922368a1bdf195a3a0e2df117
Nagesh <nageshc1992@gmail.com>
2018-07-21 15:41:45 +0530

    Removing usb_struct and updating id structure.

--- Task05/skeleton_usb.c
+++ Task05/skeleton_usb.c
@@ -2,57 +2,35 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/usb.h>
+#include <linux/hid.h>
 
-#define AUTHOR "Nagesh"
-#define DESC "An USB Driver"
-
-static int skel_usb_probe(struct usb_interface *intf,
-			  const struct usb_device_id *id)
-{
-	pr_alert("USB connected to system.\n");
-	return (0);
-}
-
-static void skel_usb_disconnect(struct usb_interface *intf)
-{
-	pr_alert("USB disconnected from system.\n");
-}
+#define AUTHOR		"3adf6a67133f"
+#define LICENSE		"GPL"
+#define DESCRIPTION	"An USB Driver"
 
 static const struct usb_device_id id_table[] = {
-	{.driver_info = 42},
+	{USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
+			    USB_INTERFACE_SUBCLASS_BOOT,
+			    USB_INTERFACE_PROTOCOL_KEYBOARD)},
 	{}
 };
 
-static struct usb_driver skel_usb_driver = {
-	.name = "skeleton_usb_driver",
-	.id_table = id_table,
-	.probe = skel_usb_probe,
-	.disconnect = skel_usb_disconnect,
-};
+MODULE_DEVICE_TABLE(usb, id_table);
 
 static int __init usb_skel_init(void)
 {
-	int ret;
-
-	pr_alert(KERN_WARNING"hello!! world\n");
-	/* register this driver with USB subsystem. */
-	ret = usb_register(&skel_usb_driver);
-	if (ret)
-		pr_alert("usb_register failed: %d\n", ret);
-
+	pr_alert("hello!! world\n");
 	return (0);
 }
 
 static void __exit usb_skel_cleanup(void)
 {
-	pr_alert(KERN_WARNING"bye!! world\n");
-	/* deregister this driver from USB subsystem. */
-	usb_deregister(&skel_usb_driver);
+	pr_alert("bye!! world\n");
 }
 
 module_init(usb_skel_init);
 module_exit(usb_skel_cleanup);
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE(LICENSE);
 MODULE_AUTHOR(AUTHOR);
-MODULE_DESCRIPTION(DESC);
+MODULE_DESCRIPTION(DESCRIPTION);
